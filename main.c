bfbf4215c3ab337076fba091dbf520823216a02a
sdp8483 <17242713+sdp8483@users.noreply.github.com>
2021-03-21 19:48:15 -0400

    update register access

--- main.c
+++ main.c
@@ -1,34 +1,30 @@
-// #include <atmel_start.h>
 #include <sam.h>
-// #include <samc21.h>
-
-#define REG_PORTA_PMUX7 (*(RwReg8 *) 0x41000037UL)    /* PortA pin mux register address for PA14 */
-#define REG_PA14_PINCFG (*(RwReg8 *) 0x4100004eUL)    /* PortA pin config register addres for PA14 */
 
 int main(void) {
 	/* Initializes MCU, drivers and middleware */
-	// atmel_start_init();
+	// atmel_start_init();	/* don't use Atmel Start Code */
 
-	/* change 48MHz Oscillator divider from default of 0b1011, see pg 214 of datasheet */
-	REG_OSCCTRL_OSC48MDIV = 0b0001;
-	while (REG_OSCCTRL_OSC48MSYNCBUSY != 0); // wait? may not be necessary
+	/* Initialize Clock ------------------------------------------------------*/
+	NVMCTRL->CTRLB.bit.RWS = 1;	/* 1 wait state required for 48MHz clock */
 
-	/* Output GCLK to a GPIO so we can check the frequency 
-	 * PA14 is used for GCLK_IO[0] peripheral function H (0x7), page 28 and 457 of datasheet
-	 * By Default the internal 48MHz oscillator is divided by 12 so pin should cycle at a freqency of 4MHz when REG_OSCCTRL_OSC48MDIV = 0b1011
-	 * When REG_OSCCTRL_OSC48MDIV = 0b0001 frequency should be 24MHz
-	 * Setting REG_OSCCTRL_OSC48MDIV = 0b0000 should give full 48MHz
-	 */
-	REG_GCLK_GENCTRL0 |= (1 << 11);
-	REG_PORTA_PMUX7 |= 0x7;
-	REG_PA14_PINCFG |= 1;
+	/* change 48MHz Oscillator divider from default of 0b1011, see pg214 of datasheet */
+	OSCCTRL->OSC48MDIV.bit.DIV = 0b0000;
 
-	/* Set PA15 as output, onboard LED is connected */
-	REG_PORT_DIR0 |= (1 << 15);
+	/* enable clock output to a corresponding GCLK_IO[0] */
+	GCLK->GENCTRL->bit.OE = 1;
+
+	/* peripheral function H for PA14, this enables GCLK_IO output, pg29 and pg457 of datasheet */
+	PORT->Group[0].PMUX[7].bit.PMUXE = 7;
+
+	/* enable peripheral multiplexer selection */
+	PORT->Group[0].PINCFG[14].bit.PMUXEN = 1;
+
+	/* GPIO setup ------------------------------------------------------------*/
+	PORT->Group[0].DIRSET.reg = PORT_PA15;	/* Set PA15 as output, onboard LED is connected */
 
 	while (1) {
 		/* this little LED of mine, I'm going to let it blink */
-		REG_PORT_OUTTGL0 |= (1 << 15);
-		for(uint32_t i=0; i<5000000UL; i++);
+		PORT->Group[0].OUTTGL.reg = PORT_PA15;
+		for(uint32_t i=0; i<5000000UL; i++);	/* delay */
 	}
 }
